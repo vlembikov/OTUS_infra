17229b3f0f30add43b1f68d239683a6f08a98413
vlembikov <vlembikov@gmail.com>
2021-02-12 10:40:56 +0300

    Fix install mongodb

--- startup-script.sh
+++ startup-script.sh
@@ -6,8 +6,8 @@ sudo apt upgrade -y
 sudo apt install -y ruby-full ruby-bundler build-essential
 
 #Install mongodb
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E52529D4
-sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.0.list'
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt update
 sudo apt install mongodb-org -y
 sudo systemctl start mongod
