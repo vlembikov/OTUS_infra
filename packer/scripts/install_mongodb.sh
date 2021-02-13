66bdacca7711b07b4a5c9f25bd6cff182ae08bac
Vladimir Lembikov <vlembikov@gmail.com>
2021-02-13 16:18:53 +0300

    task packer

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Install MongoDB
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get update
+apt-get install -y mongodb-org
+systemctl enable mongod
